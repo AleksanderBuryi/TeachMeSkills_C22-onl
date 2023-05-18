0772a468295e0696ce0ed07f8f966d76a3d9d8b0
Александр Бурый <a.buriy@it-minsk.by>
2023-05-18 10:05:56 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Test");
+        System.out.println();// print empty string
+
+        //writes a sentence in one line
+        System.out.print("This is ");
+        System.out.print("the use of");
+        System.out.print(" \"print()\" ");
+        System.out.print("in one line.");
+        System.out.print("\n"); // use enter or carriage return
+        System.out.print("On new string.");
+        System.out.print("\n"); // use enter or carriage return
+        System.out.print(""); // print "nothing". System.out.print() - error
+        System.out.print("\n");
+
+        //writes a sentence in multiple lines
+        System.out.println("This is the ");
+        System.out.println("use of \"println()\" ");
+        System.out.println("in multiple lines.");
+
+    }
+}
