b724e08d01623ce02adbaebc773c35ad9ddd7165
Александр Бурый <a.buriy@it-minsk.by>
2023-06-30 10:34:21 +0300

    Homework #12. Additional task. Task 2 init.

---
+++ src/task2/Main/Main.java
@@ -0,0 +1,7 @@
+package task2.Main;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\n\t\t\t === Homework 12. Additional task. Task 2 ===\n");
+    }
+}
