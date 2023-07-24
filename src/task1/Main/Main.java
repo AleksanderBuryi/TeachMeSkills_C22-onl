47682d2251f6ab08b6b877e1212ab1fa9788afb2
Александр Бурый <a.buriy@it-minsk.by>
2023-07-24 09:02:20 +0300

    Homework #13 init.

--- src/task1/Main/Main.java
+++ src/task1/Main/Main.java
@@ -1,10 +1,7 @@
 package task1.Main;
-import static task1.classes.DocumentsNumbersFile.readFile;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("\n\t\t\t === Homework 12. Main task. Task 1 ===\n");
-
-        readFile(); // path is src/task1/resources/DocumentsNumbers.txt
+        System.out.println("\n\t\t\t === Homework 13. Main task. Task 1 ===\n");
     }
 }
